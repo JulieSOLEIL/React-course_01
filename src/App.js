4abef2fe5f42d3ff1e0dec5d99cb73ede5aeeedd
SUN Julie <sun.julie0@gmail.com>
2022-02-20 23:50:22 +0100

    erreur meetups

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import Todo from './components/Todo';
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
-  return <div>
-    <h1>My Todos</h1>
-    <Todo text='Learn first' />
-    <Todo text='Read second' />
-    <Todo text='Try third' />
-  </div>;
+  return (
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={ <AllMeetupsPage /> } />
+        <Route path="/new-meetup" element={ <NewMeetupPage /> } />
+        <Route path="/favorites" element={ <FavoritesPage /> } />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
- 
