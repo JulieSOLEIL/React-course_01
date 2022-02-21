f512277c365ab13fd6e581317cceae3011e7b4a4
Julie SUN <sun.julie0@gmail.com>
2022-02-21 12:13:01 +0100

    maj layout & newMeetupForm

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,15 @@
+import Card from '../ui/Card';
+import classes from './NewMeetupForm.module.css';
+
+function NewMeetupForm() {
+    return <Card>
+        <form className={classes.form}>
+            <div className={classes.control}>
+                <label htmlFor=''>Meetup Title</label>
+                <input type="text" required id='title' />
+            </div>
+        </form>
+    </Card>
+}
+
+export default NewMeetupForm;
